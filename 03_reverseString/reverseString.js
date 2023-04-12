1b09d36a984d8af5a356525f1bbda4d5c86f160e
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-12 13:50:31 +0100

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let newString = stringArray.reverse().join("");
+    return newString;
 };
 
 // Do not edit below this line
