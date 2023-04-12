43510605db020ae51d10433e76c14340d4d4a0a5
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-12 14:25:54 +0100

    Add contingency for values that do not exist in the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function(array,...args) {
     for (let i = 0; i < args.length; i++) {
+        // if the value specified to be removed from the array does not exist
+        if (array.indexOf(args[i]) === -1) {
+            continue
+        }
         array.splice(array.indexOf(args[i]),1);
     }
     return array;
