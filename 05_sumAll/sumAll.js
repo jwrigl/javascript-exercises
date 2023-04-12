75a8d66b83ccf6c995d2ed249aa6e0ae3259fcf5
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-12 15:19:20 +0100

    Add handling for negative arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
 const sumAll = function(start,end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR'
+    }
+
+    
     let sum = 0;
     for (let i = start; i <= end; i++) {
         sum += i
