43aca59f448df34fceb70544ca24916d11101b9e
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-12 12:58:36 +0100

    Complete Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if (num === 0){
+        return "";
+    }
+    
+    else if (num < 0) {
+        return "ERROR"
+    }
 
+    let newstring = string;
+    for (let i = 1; i < num; i++) {
+        newstring += string;
+    }
+
+    return newstring;
 };
 
 // Do not edit below this line
