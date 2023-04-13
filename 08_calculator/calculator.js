ba0a4b6dbb49d16eb97285e37a7104ed051a48b6
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-13 15:52:06 +0100

    Complete exercise 08.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
+const add = function(addOne,addTwo) {
+	return addOne + addTwo;
+};
+
+const subtract = function(subtractOne,subtractTwo) {
+  return subtractOne - subtractTwo;
 	
 };
 
-const subtract = function() {
+const sum = function(sumArray) {
+  let total = 0;
+  for (i=0;i<sumArray.length;i++) {
+    total += sumArray[i];
+  }
+  return total;
+};
+
+const multiply = function(multiplyarray) {
+  result = multiplyarray[0]
+  for (i=1;i<multiplyarray.length;i++) {
+    result = result*multiplyarray[i];
+
+  }
+  return result;
+};
+
+const power = function(base,exponent) {
+  return base**exponent;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(factorialNumber) {
+  if (factorialNumber === 0) {
+    return 1
+  }
+  let result = factorialNumber;
+  for (i=factorialNumber-1;i>1;i--) {
+    result = result*i;
+  }
+  return result;
 	
 };
 
