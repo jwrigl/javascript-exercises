e8f19eeae119aeab287742612c1e3c2ca291f865
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-13 18:01:56 +0100

    Use reduce to convert array to string. Filter out all punctuation and whitespace. Unfixed bug where if first character is punctuation it will not be filtered

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,15 @@
 const palindromes = function (string) {
-    stringArray = string.split("")
-    if (stringArray === stringArray.reverse()) {
-        return true;
-    }
-    else {
-        return false;
-    }
-
+    let stringArray = string.split("");
+    console.log(stringArray);
+    stringArray = stringArray.reverse();
+    console.log(stringArray);
+    reversedString = stringArray.reduce((accumulator, currentValue, currentIndex) => {
+        return accumulator + (currentValue.includes(" ",",",".","!") ? "" : currentValue);
+    }, "");
+    console.log(reversedString);
+    return string === reversedString; 
 };
 
+palindromes('Animal loots foliated detail of stool lamina.')
 // Do not edit below this line
 module.exports = palindromes;
