a75cd63deb638a05ba5dbb93b7fa820394176c60
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-13 18:52:10 +0100

    Create basic structure for producing fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+        return number <= 0 ? "OOPS" :
+            number === 1 || number === 2 ? 1 :
+            (() => {
+                let fibOne = 1;
+                let fibTwo = 2;
+                for (i=3; i<number;i++) {
+                    let temp = fibTwo + fibOne;
+                    fibOne = fibTwo;
+                    fibTwo = temp;
+                 };
+                return fibTwo; })();
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
