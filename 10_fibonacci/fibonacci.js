7e4c2ab7699a0c28a1a9a2e05caef9dae3e3f5c2
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-13 19:03:19 +0100

    Add contigency for numbers entered as strings. Fix bug causing results to be higher in fibonacci systems than expected.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,9 @@
 const fibonacci = function(number) {
+        number = parseInt(number);
+        // returns oops if number negative or zero, else it will return 1 if the number is one or two
         return number <= 0 ? "OOPS" :
             number === 1 || number === 2 ? 1 :
+            // if none of these conditions are met IIFE will return the chosen fibonacci number
             (() => {
                 let fibOne = 1;
                 let fibTwo = 2;
@@ -9,7 +12,8 @@ const fibonacci = function(number) {
                     fibOne = fibTwo;
                     fibTwo = temp;
                  };
-                return fibTwo; })();
+                return fibTwo; })(); // after function defined, it is immediately invoked by the double brackets
+                // function no longer usable after 
 
 };
 
