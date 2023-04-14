958580840b2b8b9e343ecfb82a4fb28929b98b11
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-14 12:24:57 +0100

    Outline basic structure

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    currentDate = Date();
+    currentYear = currentDate.getFullYear;
+    let oldestAge = 0;
+    let oldestPerson = ''
+    console.log(people.yearOfBirth)
+
+    for (let i = 0; i < people.length-1; i++) {
+        let age = people.yearOfBirth - people.yearOfDeath;
+        console.log(age)
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = people.name;
+            console.log(oldestPerson);
+
+        }  
+    }
+    return oldestPerson;
 
 };
 
