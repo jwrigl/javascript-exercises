30a24fb7e765df9e56e96d1ff1a66f8a2187aaae
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-14 11:45:46 +0100

    Add basic structure

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookList) {
+    let flattened = bookList.flat();
+    let bookTitles = []
+    console.log(bookList.length);
 
+    for (let i = 1; i<bookList.length-1;i ++) {
+            let bookDetails = bookList[i].title
+            bookTitles.push(bookDetails[1])
+    }
+
+    return bookTitles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
