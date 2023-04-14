cdccf0796427047f49827eee21e642aef58f57fb
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-14 11:57:06 +0100

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,16 +1,11 @@
 const getTheTitles = function(bookList) {
     let flattened = bookList.flat();
     let bookTitles = []
-    console.log(bookList.length);
+    for (let i=0;i<flattened.length;i++) {
+        bookTitles.push(flattened[i].title)
 
-    for (let i = 1; i<bookList.length-1;i ++) {
-            let bookDetails = bookList[i].title
-            bookTitles.push(bookDetails[1])
     }
-
     return bookTitles;
 };
-
-
 // Do not edit below this line
 module.exports = getTheTitles;
